699410234629091cbbf2bd81bd5256b421cd5aa6
cristidana <47077247+cristidana@users.noreply.github.com>
2025-03-01 14:21:17 +0200

    Equip Item

---
+++ Assets/Scripts/EquipItem.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EquipItem : MonoBehaviour
+{
+    public Transform weaponSlot;
+    public GameObject currentWeapon;
+    public PlayerController pc;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            GameObject newWeapon = other.gameObject;
+
+            Vector3 currentWeaponPosition = currentWeapon.transform.position;
+            Quaternion currentWeaponRotation = currentWeapon.transform.rotation;
+
+            Destroy(currentWeapon);
+            Destroy(other.gameObject);
+
+            currentWeapon = Instantiate(newWeapon, currentWeaponPosition, currentWeaponRotation);
+
+            currentWeapon.transform.SetParent(weaponSlot);
+
+            Animator weaponAnimator = currentWeapon.GetComponent<Animator>();
+            weaponAnimator.enabled = true;
+            pc.arma = currentWeapon;
+
+
+        }
+    }
+}
